03ff3fa23952f8c47a6741a360425560e74771ff
Simon Struthers <snstruthers@gmail.com>
2020-08-17 17:57:09 -0700

    [10.3] Finished quick assignments

---
+++ 10.3/es2015.js
@@ -0,0 +1,5 @@
+const double = (arr) => arr.map((val) => val * 2);
+
+const squareAndFindEvens = (numbers) =>
+    numbers.map((num) => num ** 2).filter((square) => square % 2 === 0)
+;
---
+++ 10.3/es5.js
@@ -0,0 +1,15 @@
+function double(arr) {
+    return arr.map(function(val) {
+        return val * 2;
+    });
+}
+
+function squareAndFindEvens(numbers){
+    var squares = numbers.map(function(num){
+      return num ** 2;
+    });
+    var evens = squares.filter(function(square){
+      return square % 2 === 0;
+    });
+    return evens;
+  }
